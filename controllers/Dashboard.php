d0a1d313b5402b0c64bafc1f1a1557f5c6c52de1
Aram Hamo <contact@aramhamo.me>
2023-11-13 08:38:35 +0100

    modified:   controllers/Dashboard.php

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -1,10 +1,20 @@
 <?php declare(strict_types=1);
 namespace AramHamo\Mvc\Controllers;
 use AramHamo\MvcCore\View;
+use AramHamo\Mvc\Models\Url;
+use AramHamo\MvcCore\Middleware;
 
-class Dashboard{
+class Dashboard extends Middleware{
   public function get(){
-    include "../middlewares/auth.php";
+    $this->middleware('auth');
+
+  if(isset($_POST['short'])){
+    $url = new Url;
+    $url->url = $_POST['url'];
+    $url->username = userdata[0]["username"];
+    $url->tokan = bin2hex(random_bytes(5));
+    $url->create();
+  }
     return View::render("dashboard",array("title"=>"Dashboard"));
   }
   public function post(){
